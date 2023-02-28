d9bb8af40cbf0f6cacf408ad5e0148eb339820a0
Sergei <korevskiy_s@mail.ru>
2023-03-01 00:51:32 +0300

    Сделано все согласно Спринт 4/14: 4 → Тема 2/4: Вёрстка по макету → Урок 5/5

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,7 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
       print("Hello world")
-      sleep(3)
+     // sleep(3)
         return true
     }
 
