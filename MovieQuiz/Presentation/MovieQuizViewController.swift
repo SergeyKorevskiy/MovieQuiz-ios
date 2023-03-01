9f874d1718be2874955ac187f1853b52594f8aa5
Sergei <korevskiy_s@mail.ru>
2023-03-01 21:30:39 +0300

    Спринт 4/14: 4 → Тема 3/4: Реализация логики по макету → Урок 4/4 Обработка ответа пользователя - мое решение

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,10 +2,166 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
+  
+  
+  
+  @IBAction private func noButtonClicked(_ sender: UIButton) {
+      showAnswerResult(isCorrect: false)
+      currentQuestionIndex += 1
+  }
+  
+  @IBAction private func yesButtonClicked(_ sender: UIButton) {
+    showAnswerResult(isCorrect: true)
+    currentQuestionIndex += 1
+  }
+  
+  @IBOutlet private weak var counterLabel: UILabel!
+  
+  @IBOutlet private weak var imageView: UIImageView!
+  
+  @IBOutlet  private weak var textLabel: UILabel!
+  
+  
+  // для состояния "Вопрос задан"
+  struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+  }
+
+  // для состояния "Результат квиза"
+  struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+  }
+
+  struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+  }
+
+  
+  private let questions: [QuizQuestion] = [
+          QuizQuestion(
+              image: "The Godfather",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: true),
+          QuizQuestion(
+              image: "The Dark Knight",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: true),
+          QuizQuestion(
+              image: "Kill Bill",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: true),
+          QuizQuestion(
+              image: "The Avengers",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: true),
+          QuizQuestion(
+              image: "Deadpool",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: true),
+          QuizQuestion(
+              image: "The Green Knight",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: true),
+          QuizQuestion(
+              image: "Old",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: false),
+          QuizQuestion(
+              image: "The Ice Age Adventures of Buck Wild",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: false),
+          QuizQuestion(
+              image: "Tesla",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: false),
+          QuizQuestion(
+              image: "Vivarium",
+              text: "Рейтинг этого фильма больше чем 6?",
+              correctAnswer: false)
+      ]
+  
+  private var currentQuestionIndex: Int = 0
+  
+  private func show(quiz step: QuizStepViewModel) {
+    // здесь мы заполняем нашу картинку, текст и счётчик данными
+    counterLabel.text = step.questionNumber
+    imageView.image = step.image
+    textLabel.text = step.question
+  }
+
+  private func show(quiz result: QuizResultsViewModel) {
+    // здесь мы показываем результат прохождения квиза
+  }
+  
+  
+  private func convert(model: QuizQuestion) -> QuizStepViewModel {
+      return QuizStepViewModel(
+          image: UIImage(named: model.image) ?? UIImage(), // распаковываем картинку
+          question: model.text, // берём текст вопроса
+          questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // высчитываем номер вопроса
+    
+  //  UIImage(imageLiteralResourceName: model.image
+  }
+
+  private func showAnswerResult(isCorrect: Bool) {
+    if isCorrect == questions[currentQuestionIndex].correctAnswer{
+      textLabel.text = "ВЕРНО!"
+    } else {
+      textLabel.text = "НЕВЕРНО!"
+    }
+  }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      let currentQuestion = questions[currentQuestionIndex]
+      
+      
+      show(quiz: convert(model: currentQuestion))
+      
+      
+
     }
+  
+  override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      /*
+      Тут имеет смысл дополнительно настроить наши изображения, например,
+      задать цвет фона для экрана.
+      */
+    }
+  
+  override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      /*
+      Тут имеет смысл оповестить систему аналитики о том, что экран показался.
+      */
+    }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+     super.viewWillDisappear(animated)
+     /*
+     Тут имеет смысл остановить все процессы, которые происходили на этом экране.
+     */
+   }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+     super.viewDidDisappear(animated)
+     /*
+     Тут имеет смысл оповестить систему аналитики, что экран перестал показываться
+     и привести его в изначальное состояние.
+     */
+   }
+  
+  
 }
+
 
 /*
  Mock-данные
